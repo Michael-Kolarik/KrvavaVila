e88b18ac7dbab43a8b8fce204e0b5ba540c548ed
Michael Kolařík <kolarikmichael@gmail.com>
2020-11-27 14:54:21 +0100

    Druha verze

---
+++ _READ_ME_.py
@@ -0,0 +1,11 @@
+Krvavá vila je krátká amatérská textová CRPG napsaná v jazyce Python. Je inspirována paravidly stolní hry
+Jeskyně a Draci, na které se rovněž podílím. Jedná se o hru textovou, ovládanou s pomocí klávesnice.
+
+Vývoj hry stále probíhá, ale jedná se již pouze o drobné opravy a zefektivnění, tato verze je již
+plně hratelná.
+
+Příběh Krvavé vily se odehrává ve fantasy světě. Hráč ovládá elfí zlodějku Mezzobran, která dostane zakázku,
+která se uáže ještě složitější, než se na počátku zdálo.
+
+Pokud si chcete hru zahrát, a určit, jak příběh elfí zlodějky dopadne, stáhněte si tyto soubory a spusťte
+soubor main.py
